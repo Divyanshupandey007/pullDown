0398554dff8ed869314e11618a747e73ad16e379
Divyanshupandey007 <79376921+Divyanshupandey007@users.noreply.github.com>
2026-01-05 15:14:53 +0000

    Feat: Implement single file download logic

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main(){
-	fmt.Println("Download manager")
+	res,err:=http.Get("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+	
+	if err!=nil{
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	file,err:=os.Create("dummy.pdf")
+
+	if err!=nil{
+		panic(err)
+	}
+
+	defer file.Close()
+
+	io.Copy(file,res.Body)
+
+	fmt.Println("Download Finished")
+
 }
